a92bc420c61008653ea95399d82ed738abb0bd5c
Robin Schneider <ypid@riseup.net>
2017-01-27 21:12:42 +0100

    html_use_smartypants = False to render :command:`iptables --list` as is
    Related to: https://github.com/debops/docs/pull/254

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # DebOps Contrib documentation build configuration file
-# Copyright (C) 2015-2016 DebOps Project https://debops.org/
+# Copyright (C) 2015-2017 DebOps Project https://debops.org/
 #
 # This file is execfile()d with the current directory set to its
 # containing dir.
@@ -61,7 +61,7 @@ master_doc = 'index'
 # General information about the project.
 project = u'DebOps Contrib'
 author = u'DebOps Contrib Collective'
-copyright = u'2016, {}'.format(author)
+copyright = u'2016-2017, {}'.format(author)
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -207,6 +207,8 @@ html_static_path = ['_static']
 # typographically correct entities.
 #
 # html_use_smartypants = True
+html_use_smartypants = False
+# Set to False to render :command:`iptables --list` as `iptables --list` and not `iptables –list`.
 
 # Custom sidebar templates, maps document names to template names.
 #
